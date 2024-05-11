86bb7c694ec8604edb3be0504ab0dfb7c62c1e9f
Girijaravi123 <ravigirija.2005@gmail.com>
2024-05-11 10:24:28 +0530

    first commit

--- first/server.js
+++ first/server.js
@@ -1,14 +1,33 @@
-const http = require('http')
-const  port = 8000
-const server = http.createServer(function(req, res)){
-    res.write('First node.js web server!!!'))
-    res.end()
+var inputval = document.querySelector('#cityinput')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+var apik = "3045dd712ffe6e702e3245525ac7fa38"
+function convertion(val)
+{
+    return (val - 273).toFixed(2)
+}
 
-})
-server.listen(port, function(error)){
-    if(error) {
-        console.log('something wrong ||')
-    } else{
-        console.log('Server is listening to:'+ port)
-    }
+btn.addEventListener('click', function()
+{
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
+  .then(res => res.json())
+
+
+  .then(data => 
+  {
+    var nameval = data['name']
+    var descrip = data['weather']['0']['description']
+    var tempature = data['main']['temp']
+    var wndspd = data['wind']['speed']
+    city.innerHTML=`Weather of <span>${nameval}<span>`
+    temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
+    description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
+    wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
+
+  })
+
+  .catch(err => alert('You entered Wrong city name'))
 })
